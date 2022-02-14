e666e5975bc36446164d90ab95b1ee972e03b4ad
Martin Knudsen <martinknud@gmail.com>
2022-02-14 09:00:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gangetabeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hvilket tal vil du have gangetabeller op til?");
+            int tal = Int32.Parse(Console.ReadLine());
+            for (var i = 1; i <= tal; i++)
+            {
+                string tabel = "";
+                for (var j = 1; j < 11; j++)
+                {
+                    tabel += i + "x" + j + " = " + i * j + " "; 
+                }
+                Console.WriteLine(tabel);
+            }
+        }
+    }
+}
